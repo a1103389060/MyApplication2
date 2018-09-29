203fc4b6385873532dfe5cf4ee02c5d720ae877a
Your Name <you@example.com>
2018-09-29 15:39:20 +0800

    a

--- app/src/main/java/com/example/a11033/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/a11033/myapplication/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.a11033.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +10,7 @@ import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
     private static final String TAG = "you clicked here";
-
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,34 +20,52 @@ public class SecondActivity extends AppCompatActivity {
         Button btn_stop;
         Button btn_unbind;
         Button btn_bind;
+        Button btn_create;
+
+        setContentView(R.layout.activity_second);
         {
             btn_start =(Button) findViewById(R.id. button_2_1);
             btn_stop = (Button) findViewById(R.id.button_2_2);
             btn_bind = (Button) findViewById(R.id.button_2_3);
             btn_unbind = (Button) findViewById(R.id.button_2_4);
+            btn_create = (Button) findViewById(R.id.button_2_5);
         }
-        setContentView(R.layout.activity_second);
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG, "onClick: StatrService");
+                Log.d(TAG, "onClick: StartService");
+                intent = new Intent(SecondActivity.this,MyService.class);
             }
         });
         btn_stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "onClick: StopService");
+                intent = new Intent(SecondActivity.this,MyService.class);
 
             }
         });
         btn_bind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "onClick: bindService");
+                intent = new Intent(SecondActivity.this,MyService.class);
 
             }
         });
         btn_unbind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "onClick: UnbindService");
+                intent = new Intent(SecondActivity.this,MyService.class);
+
+            }
+        });
+        btn_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: createService");
+                intent = new Intent(SecondActivity.this,MyService.class);
 
             }
         });
